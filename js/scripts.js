5ba9c63bbb9069e02e3caeac7db7de4a5426835b
Trent Petersen <Foxtrot@Foxtrot.local>
2014-05-30 08:25:00 -0700

    slider added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
 $(document).foundation();
+
+$(document).ready(function(){
+
+	$('.center-slick').slick({
+	  centerMode: true,
+	  centerPadding: '60px',
+	  slidesToShow: 3,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: true,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: true,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+
+});
